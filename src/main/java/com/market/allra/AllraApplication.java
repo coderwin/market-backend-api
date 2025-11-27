bfeb92055d2408dea67d18691a98a12e1beba2e4
devlhj <devlhj@naver.com>
2025-11-28 03:40:00 +0900

    first init

---
+++ src/main/java/com/market/allra/AllraApplication.java
@@ -0,0 +1,13 @@
+package com.market.allra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/allra/AllraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.allra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
